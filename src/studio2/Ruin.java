eeef7c7c47ec9aba4886056aa25092ab23a8b139
josephzhang <josephzhang@josephs-mbp.dhcp.wustl.edu>
2023-02-02 12:51:06 -0600

    Doug

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Ruin {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
-		
+
+		//Scanners for variables
 		System.out.println("Enter your start amount: ");
 		double startAmount = in.nextDouble();
 		System.out.println("Enter your win chance: ");
@@ -14,9 +14,22 @@ public class Ruin {
 		System.out.println("Enter your win limit: ");
 		double winLimit = in.nextDouble();
 		double wallet = startAmount;
-		for (int x = 1; x <= 6; x++)
+		//Algorithm for expected ruin rate
+		double expectedRuin;
+		double alpha = (1-winChance)/winChance;
+		if (winChance == 0.5)
 		{
+			expectedRuin = 1 - (startAmount/winLimit);
+		}
+		else
+		{
+			expectedRuin = (Math.pow(alpha, startAmount);
+		}
 		
+		//Code logic for running gambling system
+		for (int totalSimulations = 1; totalSimulations <= 6; totalSimulations++)
+		{
+			System.out.println("Start of day: " + totalSimulations);
 			while (wallet < winLimit && wallet > 0) 
 			{
 				double calculator = Math.random();
@@ -33,7 +46,7 @@ public class Ruin {
 					System.out.println("Your balance is: $" + wallet);
 				}
 			}
-		
+
 			if (wallet == winLimit)
 			{
 				System.out.println("Your day was a success!");
@@ -42,8 +55,11 @@ public class Ruin {
 			{
 				System.out.println("Your day was a ruin...");
 			}
-			System.out.println("End of day  " + x);
+			System.out.println("End of day  " + totalSimulations);
 			wallet = startAmount;
+			System.out.println("----------------------------");
 		}
-		}
+		
+
 	}
+}
